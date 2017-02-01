c24b976a16ada2817e3f400d7c2efc1e854d97b2
Motoharu Yano <motoharu@linux-16uu.Dlink>
2017-02-02 03:14:58 +0400

    add game card read/write export definitions

--- driver/psvemmc.h
+++ driver/psvemmc.h
@@ -18,4 +18,12 @@ int readClusterMs(int cluster, char* buffer);
 
 int writeClusterMs(int cluster, char* buffer);
 
+int readSectorGc(int sector, char* buffer);
+
+int writeSectorGc(int sector, char* buffer);
+
+int readClusterGc(int cluster, char* buffer);
+
+int writeClusterGc(int cluster, char* buffer);
+
 int psvemmcDeinitialize();
