71176604aa27048ab41bfe81e95691a909d4364a
Motoharu Yano <motoharu@yano.com>
2017-06-03 17:26:00 +0400

    add new types

--- driver/vfs_types.h
+++ driver/vfs_types.h
@@ -115,74 +115,163 @@ typedef struct vfs_device_info //size is 0xC
    uint32_t unk_8;
 }vfs_device_info;
 
+typedef struct vfs_mount_cc
+{
+   uint32_t unk_0;
+   uint32_t unk_4;
+   uint32_t unk_8; // pointer to data section, offset 34
+   uint32_t unk_C; // pointer to data section, offset 1C
+} vfs_mount_cc;
+
+struct vfs_node;
+
+typedef struct vfs_fd_lock
+{
+   SceUID mutex_SceVfsFdLock; // 0
+   SceUID cond_SceVfsFdCond; // 4
+   uint32_t unk_8;
+   uint32_t unk_C;
+
+} vfs_fd_lock;
+
+typedef struct vfs_mount //size is not known exactly, at least 0xD0
+{
+   uint32_t fast_mutex_SceVfsMnt;
+
+   uint32_t unk_4;
+   uint32_t unk_8;
+   uint32_t unk_C;
+
+   uint8_t data1[0x30];
+
+   struct vfs_node* unk_40; // child ?
+
+   SceUID pool;     // 0x44 - SceIoVfsHeap
+
+   uint32_t unk_48; // = 0x101
+   
+   union _devMajor
+   {
+      struct _dmDword
+      {
+         uint32_t unk_4C;
+      } dw;
+      struct _dmWord
+      {
+         uint16_t unk_4C;
+         uint16_t unk_4E;
+      } w;
+      struct _dmByte
+      {
+         uint8_t unk_4C;
+         uint8_t unk_4D;
+         uint8_t unk_4E;
+         uint8_t unk_4F;
+      } b;
+   } devMajor;
+
+   uint32_t devMinor; // 0x50
+
+   struct vfs_node* unk_54;
+   uint32_t unk_58;   // counter
+   struct vfs_add_data* add_data; // 0x5C
+
+   uint32_t unk_60;   // counter
+
+   uint32_t unk_64;
+   uint32_t unk_68;
+   uint32_t unk_6C;
+
+   struct vfs_mount* unk_70; // next ?
+   struct vfs_mount* unk_74; // prev ?
+
+   struct vfs_mount* unk_78; //singly linked list related to pointer 19D0 in data section
+
+   struct vfs_block_dev_info* blockDev; // 0x7C
+
+   char unixMount[0x40]; // unixMount /gro/exfat, ux/exfat etc
+
+   uint32_t unk_C0;   
+
+   uint32_t unk_C4; // = mountInfo->unk_14
+
+   vfs_fd_lock* fd_lock_ptr; // C8 - points to area in this structure
+
+   vfs_mount_cc* unk_CC; // CC - points to area in this structure
+
+   vfs_fd_lock fd_lock; // D0
+
+   vfs_mount_cc unk_E0; //E0;
+
+} vfs_mount;
+
+typedef struct vfs_node_70
+{
+   uint32_t unk_0;
+   uint32_t unk_4;
+   uint32_t unk_8;
+   struct vfs_node_70* unk_C; //zero or pointer to next element
+
+   void* unk_10; //pointer to struct of at least size 0x94
+   uint32_t unk_14; //number
+
+   uint32_t str_len_18;
+   char* str_1C;
+} vfs_node_70;
+
 typedef struct vfs_node
 {
    uint32_t unk_0;
-   uint32_t unk_4; //uid ?, not ptr
-   uint32_t unk_8;
-   uint32_t unk_C; //uid ?, not ptr
+   uint32_t unk_4;  // most likely SceUID of current thread
+   uint32_t unk_8;  // counter
+   SceUID event_flag_SceVfsVnode; // 0xC - event flag SceVfsVnode
 
-   uint32_t unk_10; //num
+   uint32_t evid_bits; // 0x10
    uint32_t unk_14;
    uint32_t unk_18;
    uint32_t unk_1C;
 
-   uint32_t unk_20;
-   uint32_t unk_24;
-   uint32_t unk_28;
-   uint32_t unk_2C;
-
-   uint32_t unk_30;
-   uint32_t unk_34;
-   uint32_t unk_38;
-   uint32_t unk_3C;
-
-   node_ops2 *ops;
+   uint8_t data1[0x20];
+   
+   node_ops2 *ops;  // 0x40
    uint32_t unk_44;
-   void* dev_info;
-   uint32_t unk_4C; // not a pointer
+   void* dev_info;  //allocated on heap with uid from uid field
+                    //this is device specific / node specific data
+                    //for partition node this will be vfs_device_info*
 
-   struct vfs_node* prev_node;
-   vfs_node_unk_54* unk_54; //ptr
-   uint32_t unk_58; //num
+   vfs_mount* node; // 0x4C
+
+   struct vfs_node* prev_node; // 0x50
+
+   struct vfs_node* unk_54; // copied from node base
+   uint32_t unk_58;  // counter - probably counter of nodes
    uint32_t unk_5C;
 
    uint32_t unk_60;
    uint32_t unk_64;
    uint32_t unk_68;
-   SceUID pool_uid;
+   SceUID pool_uid;  // 0x6C - SceIoVfsHeap or other pool
 
-   vfs_node_unk_70* unk_70; //ptr
-   uint32_t unk_74; //num
-   uint32_t unk_78; //num
+   vfs_node_70* unk_70;
+   uint32_t unk_74; // = 0x8000
+   uint32_t unk_78; // some flag
    uint32_t unk_7C;
 
-   uint32_t unk_80; //ptr ?
+   uint32_t unk_80;
    uint32_t unk_84;
-   uint32_t unk_88; //num
+   uint32_t unk_88;
    uint32_t unk_8C;
 
-   uint32_t unk_90; //num
-   uint32_t unk_94; //num
+   uint32_t unk_90;
+   uint32_t unk_94;
    uint32_t unk_98;
    uint32_t unk_9C;
 
-   uint32_t unk_A0;
-   uint32_t unk_A4;
-   uint32_t unk_A8;
-   uint32_t unk_AC;
+   uint8_t data2[0x30];
+   
+   uint32_t unk_D0; //cur_node->devMajor.w.unk_4E
 
-   uint32_t unk_B0;
-   uint32_t unk_B4;
-   uint32_t unk_B8;
-   uint32_t unk_BC;
-
-   uint32_t unk_C0;
-   uint32_t unk_C4;
-   uint32_t unk_C8;
-   uint32_t unk_CC;
-
-   uint32_t unk_D0; //num
+   uint8_t data3[0x2C];
 }vfs_node;
 
 typedef struct node_ops1 // size is 0x34 (13 pointers)
@@ -253,5 +342,111 @@ typedef struct vfs_node_info //size is 0x38
    struct vfs_node_info* unk_30; //can be zero
    struct vfs_node_info* unk_34; //can be zero
 }vfs_node_info;
+
+//-------
+
+struct io_scheduler;
+
+typedef struct io_scheduler_dispatcher //size is 0xB8
+{
+   char unk_0[0x50];
+
+   struct io_scheduler* unk_50; // Internal
+   struct io_scheduler* unk_54; // Game Card
+   struct io_scheduler* unk_58; // Removable
+   struct io_scheduler* unk_5C; // Host File System
+   struct io_scheduler* unk_60; // Default
+   
+   uint32_t unk_64; // pointer to unknown module data section
+   uint32_t unk_68; //SceIoSchedulerDispatcher fast mutex
+   uint32_t unk_6C;
+   
+   char unk_70[0x30];
+   
+   uint32_t unk_A0;
+   uint32_t unk_A4;
+   
+   SceUID unk_A8; //SceIoSchedulerDispatcher event flag uid
+   uint32_t unk_AC; // counter
+   uint32_t unk_B0; // flag
+   uint32_t unk_B4; // 0
+   
+} io_scheduler_dispatcher;
+
+typedef struct io_scheduler_item //size is 0x14 - allocated from SceIoScheduler heap
+{
+   struct io_scheduler* unk_0; // parent
+   uint32_t unk_4; // 0
+   uint32_t unk_8; // 0
+   uint32_t unk_C; // 0
+   uint32_t unk_10; // pointer to unknown module data section
+}io_scheduler_item;
+
+typedef struct io_scheduler //size is 0xA8 - allocated from SceIoScheduler heap
+{
+   uint32_t unk_0; //code: 0x100, 0x201, 0x202, 0x300, 0x0
+                  //Internal, Game Card, Removable, Host File System, Default
+                  
+   struct io_scheduler_item* unk_4; //item 0
+   struct io_scheduler_item* unk_8; //item 1
+   struct io_scheduler_item* unk_C; //item 2
+   
+   struct io_scheduler_item* unk_10; //item 3
+   struct io_scheduler_item* unk_14; //item 4
+   struct io_scheduler_item* unk_18; //item 5
+   struct io_scheduler_item* unk_1C; //item 6
+   
+   struct io_scheduler_item* unk_20; //item 7
+   struct io_scheduler_item* unk_24; //item 8
+   struct io_scheduler_item* unk_28; //item 9
+   struct io_scheduler_item* unk_2C; //item 10
+   
+   struct io_scheduler_item* unk_30; //item 11
+   struct io_scheduler_item* unk_34; //item 12
+   struct io_scheduler_item* unk_38; //item 13
+   struct io_scheduler_item* unk_3C; //item 14
+   struct io_scheduler_item* unk_40; //item 15
+   
+   uint32_t unk_44; // flag1 0x40000 or 0x00
+   uint32_t unk_48; // flag2 0x40000 or 0x00
+   struct io_scheduler_dispatcher* unk_4C; // parent
+   
+   SceUID unk_50; //SceIoScheduler mutex
+   SceUID unk_54; //SceIoScheduler cond
+   
+   char unk_58; // 0
+   char unk_59; // 0
+   char unk_5A; // 0
+   char unk_5B; // 0
+   
+   uint32_t unk_5C; // 0
+   
+   //this is probably separate structure
+   SceUID unk_60; //SceIoSchedWorker thread id
+   SceUID unk_64; //SceIoScheduler mutex from self
+   SceUID unk_68; //SceIoScheduler cond from self
+   struct io_scheduler* unk_6C; //pointer to self
+   
+   uint32_t unk_70; // 0
+   uint16_t unk_74; // 0
+   uint16_t unk_76; // 0
+   uint32_t unk_78; // 0
+   uint32_t unk_7C; // 0
+   
+   uint32_t unk_80; //
+   uint32_t unk_84; // some flag
+   
+   uint32_t unk_88; //0x80
+   uint32_t unk_8C; //0x80
+   
+   uint32_t unk_90; //0x80
+   uint32_t unk_94; //0x80
+   uint32_t unk_98; //0x80
+   uint32_t unk_9C; //0x80
+   
+   uint32_t unk_A0; // 0
+   uint32_t unk_A4; // 0
+
+}io_scheduler;   
 
 #pragma pack(pop)
